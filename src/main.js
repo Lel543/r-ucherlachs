3aa75ff1edf1ecbcd13811777b54b5ac5a5429f9
haiderer_p <haidererpaul@gmail.com>
2025-03-05 18:42:19 +0100

    vuetifyadddeed

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './assets/css/normalize.css';
-import './assets/css/skeleton.css';
-import './assets/css/style.css';
-import './assets/css/custom.css';
+// filepath: /c:/Users/paulh/rlachs/src/main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify' // Import Vuetify
+import './assets/css/normalize.css'
+import './assets/css/skeleton.css'
+import './assets/css/style.css'
+import './assets/css/custom.css'
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+const app = createApp(App)
+app.use(router)
+app.use(vuetify) // Use Vuetify
+app.mount('#app')
