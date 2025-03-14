8f0483453602a202084330f10ca054e9684e5959
haiderer_p <haidererpaul@gmail.com>
2025-03-14 13:38:40 +0100

    transitions, slideshow, animations, new footer, vuetify mdi icosn, about me expansion panel

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,36 +1,49 @@
 <template>
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="four columns">
-            <h5>Kontakt</h5>
-            <p>Adresse: Musterstraße 1, 12345 Musterstadt</p>
-            <p>Telefon: +49 123 456789</p>
-            <p>Email: info@musterfirma.de</p>
-          </div>
-          <div class="four columns">
-            <h5>Informationen</h5>
-            <p><a href="#">Über uns</a></p>
-            <p><a href="#">Datenschutz</a></p>
-            <p><a href="#">AGB</a></p>
-          </div>
-          <div class="four columns">
-            <h5>Social Media</h5>
-            <p><a href="#">Facebook</a></p>
-            <p><a href="#">Twitter</a></p>
-            <p><a href="#">Instagram</a></p>
-          </div>
-        </div>
-      </div>
-    </footer>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Footer'
-  };
-  </script>
-  
-  <style scoped>
+  <v-footer class="text-center d-flex flex-column ga-2 py-4" color="indigo-lighten-1">
+    <div class="d-flex ga-3">
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        density="comfortable"
+        variant="text"
+      ></v-btn>
+    </div>
 
-  </style>
+    <v-divider class="my-2" thickness="2" width="50"></v-divider>
+
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+          <v-card color="indigo-lighten-1" flat>
+            <v-card-title class="text-h5">Kontakt</v-card-title>
+            <v-card-text class="text-body-1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis
+              est labore voluptatibus!
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="6">
+          <v-card color="indigo-lighten-1" flat>
+            <v-card-title class="text-h5">Informationen</v-card-title>
+            <v-card-text class="text-body-1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis
+              est labore voluptatibus!
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-divider></v-divider>
+
+    <div>{{ new Date().getFullYear() }} — <strong>Räucherlachs</strong></div>
+  </v-footer>
+</template>
+
+<script setup>
+const icons = ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram']
+</script>
+
+<style scoped></style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,47 +1,47 @@
 <template>
-    <div>
-      <div class="row mt-1">
-        <div class="twelve columns">
-          <h1>Bestellen</h1>
-        </div>
-      </div>
-      <div class="row">
-        <div class="twelve columns">
-          <hr class="seperator">
-        </div>
-      </div>
-      <div class="row">
-        <div class="six columns">
-          <figure class="offers">
-            <img src="../../public/img/lachs_whitebg.jpg" alt="Beschreibung von Bild 1" width="400">
-            <p class="product-description">500g Norwegischer Lachs</p>
-            <figcaption class="product-price">30€</figcaption>
-            <button class="bestellen_btn">Jetzt bestellen!</button>
-          </figure>
-        </div>
-        <div class="six columns">
-          <figure class="offers">
-            <img src="../../public/img/lachs_whitebg.jpg" alt="Beschreibung von Bild 1" width="400">
-            <p class="product-description">1000g Norwegischer Lachs</p>
-            <figcaption class="product-price">50€</figcaption>
-            <button class="bestellen_btn">Jetzt bestellen!</button>
-          </figure>
-        </div>
-      </div>
-      <div class="row">
-        <div class="twelve columns">
-          <hr class="seperator">
-        </div>
+  <div>
+    <div class="row mt-1">
+      <div class="twelve columns">
+        <h1>Bestellen</h1>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ProductList'
-  };
-  </script>
-  
-  <style scoped>
+    <v-divider thickness="2"></v-divider>
+    <Toast />
+    <div class="row">
+      <div class="six columns">
+        <figure class="offers">
+          <img src="../../public/img/lachs_whitebg.jpg" alt="Beschreibung von Bild 1" width="400" />
+          <p class="product-description">500g Norwegischer Lachs</p>
+          <figcaption class="product-price">30€</figcaption>
+          <v-btn class="ma-3" @click="addToCart('500g Norwegischer Lachs')">
+            Jetzt bestellen! <v-icon icon="mdi-checkbox-marked-circle" end></v-icon>
+          </v-btn>
+        </figure>
+      </div>
+      <div class="six columns">
+        <figure class="offers">
+          <img src="../../public/img/lachs_whitebg.jpg" alt="Beschreibung von Bild 1" width="400" />
+          <p class="product-description">1000g Norwegischer Lachs</p>
+          <figcaption class="product-price">50€</figcaption>
+          <v-btn class="ma-3" @click="addToCart('1000g Norwegischer Lachs')">
+            Jetzt bestellen! <v-icon icon="mdi-checkbox-marked-circle" end></v-icon>
+          </v-btn>
+        </figure>
+      </div>
+    </div>
+    <v-divider thickness="2"></v-divider>
+  </div>
+</template>
 
-  </style>
+<script>
+
+
+export default {
+  name: 'ProductList',
+
+
+    
+}
+</script>
+
+<style scoped></style>
